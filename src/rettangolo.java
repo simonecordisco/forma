6d6e82e3ff7193102888d6458cd19406905ccc80
simonecordisco <mnlsmncrd@gmail.com>
2023-10-28 11:29:43 +0200

    Initial commit

---
+++ src/rettangolo.java
@@ -0,0 +1,17 @@
+public class rettangolo extends forma{
+
+
+    rettangolo(double larghezza, double lunghezza) {
+        super(larghezza,lunghezza);
+        this.lunghezza = lunghezza;
+        this.larghezza = larghezza;
+    }
+    public void calcolaArea() {
+        double area = lunghezza * larghezza;
+        System.out.println("Questo è il calcolo dell'area1 " + area);
+    }
+
+
+}
+
+
